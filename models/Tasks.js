01bbd5ce4ad3b7ed09ba61e809d6dcbc7312bae5
NielsMasdorp <nielsmasdorp@gmail.com>
2016-01-07 12:18:10 +0100

    deletions

--- models/Tasks.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var TaskSchema = new mongoose.Schema({
-  finished: { type: Boolean, default: false },
-  body: String,
-  note: { type: mongoose.Schema.Types.ObjectId, ref: 'Note' }
-});
-
-mongoose.model('Task', TaskSchema);
